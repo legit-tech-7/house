5402d139d6144f710264921bb5014aad5f00db74
joshua <joshuachukwuemeka52@gmail.com>
2025-12-22 06:28:27 +0100

    Updated project

--- pro/settings.py
+++ pro/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#tp!dkr52do1=7i)4z!754#f*l5gb1qam2974fv%l&8o%%+&b$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
